23f57273f16e8bb95f5e1658924acc3c696dc6f9
Manaswini <manaswinisingh441@gmail.com>
2024-11-06 12:30:08 +0530

    updated the file

---
+++ app.py
@@ -0,0 +1,10 @@
+import boto3
+client = boto3.client('ec2')
+response = client.run_instances(
+ImageId='ami-0e0e417dfa2028266',
+        InstanceType='t2.micro',
+        KeyName='project',
+        MaxCount=1,
+        MinCount=1
+
+)
